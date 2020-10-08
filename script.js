05be72c6a313666365018a0e9192b6121d7221b4
Kevin McAteer <kmcateer926@gmail.com>
2020-10-07 22:02:25 -0400

    fixed local storage, adding readme

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ for (var i = 0; i < workDayHours.length; i++) {
   var timeEl = $("<div>").addClass("row time-block");
   var hourEl = $("<div>")
     .addClass("hour col-sm-1")
-    .attr("id", "H" + workDayHours[i]);
+    .attr("id", "H" + workDayHours[i].replace(" ", "").replace(":", ""));
   var textInput = $("<input>").addClass("col-sm-9");
   var saveBtn = $("<button>").attr("class", "saveBtn fas fa-save col-1");
   timeEl.append(hourEl, textInput, saveBtn);
@@ -53,17 +53,28 @@ for (var i = 0; i < workDayHours.length; i++) {
 //   {},
 //   {},
 // ];
-saveBtn.on("click", function () {
-//   var schedule = ($(this).siblings("input").val());
-//   var savedItem = ($(this).siblings(".hour").attr("id"))
+$(".saveBtn").on("click", function () {
+
+  var schedule = ($(this).siblings(".hour").attr("id"));
+  var savedItem = ($(this).siblings("input").val());
  
-  localStorage.setItem("input", "hour");
-  console.log(saveBtn);
-  console.log("input", "hour");
+  localStorage.setItem(schedule, savedItem);
+  console.log($(this));
+  console.log(schedule, savedItem);
 //   console.log($(this).siblings("input").val());
 //   console.log($(this).siblings(".hour").attr("id"))
 });
 
+$("#H900AM").siblings("input").val(localStorage.getItem("H900AM"))
+$("#H1000AM").siblings("input").val(localStorage.getItem("H1000AM"))
+$("#H1100AM").siblings("input").val(localStorage.getItem("H1100AM"))
+$("#H1200AM").siblings("input").val(localStorage.getItem("H1200AM"))
+$("#H100PM").siblings("input").val(localStorage.getItem("H100PM"))
+$("#H200PM").siblings("input").val(localStorage.getItem("H200PM"))
+$("#H300PM").siblings("input").val(localStorage.getItem("H300PM"))
+$("#H400PM").siblings("input").val(localStorage.getItem("H400PM"))
+$("#H500PM").siblings("input").val(localStorage.getItem("H500PM"))
+console.log($("#H900AM").siblings("input"))
 // // console.log(storeSchedule);
 // // var element = target.rev("input");
 // // var textInfo = element.val();
